8a7d6615601aa52548c22359250b050a212c7201
Wirenux <louis.lesniak2@gmail.com>
2026-06-09 17:33:27 +0200

    add: background gradient + taskbar

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div id="desktop">
+    <div id="taskbar">
+      <div id="clock-widget">
+        <div class="clock">17:45</div>
+        <div class="date">Jun 09</div>
+      </div>
+    </div>
+  </div>
+`
